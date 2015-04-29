d363aa3b45fb5874076b6ba31341081331c25456
Pablo Ifran <pabloifran@gmail.com>
2015-04-29 09:41:49 -0300

    Fixing issues with default variables

--- src/angular-tooltip.js
+++ src/angular-tooltip.js
@@ -1,120 +1,162 @@
-'use strict';
+(function(window, angular, undefined) {
+  'use strict';
 
-var module = angular.module('ngTooltip', []),
-        extend = angular.extend;
+  var extend = angular.extend;
 
-module.provider('$tooltip', function() {
-    // Default template for tooltips.
-    var defaultTemplateUrl = 'template/ng-tooltip.html'
-    this.setDefaultTemplateUrl = function(templateUrl) {
-        defaultTemplateUrl = templateUrl;
-    },
-    defaultTetherOptions = {
+  angular.module('ngTooltip', [])
+    .factory('safeApply', [function() {
+      return function($scope, fn) {
+        var phase = $scope.$root.$$phase;
+        if(phase == '$apply' || phase == '$digest') {
+          if (fn) {
+            $scope.$eval(fn);
+          }
+        } else {
+          if (fn) {
+            $scope.$apply(fn);
+          } else {
+            $scope.$apply();
+          }
+        }
+      }
+    }])
+
+    .provider('$tooltip', function() {
+      // Default template for tooltips.
+      var defaultTemplateUrl = 'template/ng-tooltip.html';
+      var defaultTetherOptions = {
         attachment: 'top middle',
         targetAttachment: 'bottom middle'
-    };
+      };
 
-    this.setDefaultTetherOptions = function(options) {
+      this.setDefaultTemplateUrl = function(templateUrl) {
+        defaultTemplateUrl = templateUrl;
+      };
+
+      this.setDefaultTetherOptions = function(options) {
         extend(defaultTetherOptions, options);
-    };
+      };
 
-    this.$get = function($rootScope, $animate, $compile, $templateCache) {
+      this.$get = ['$rootScope', '$animate', '$compile', '$templateCache', function($rootScope, $animate, $compile, $templateCache) {
         return function(options) {
-            options = options || {};
-            options = extend({ templateUrl: defaultTemplateUrl }, options);
-            options.tether = extend({}, defaultTetherOptions, options.tether || {});
+          options = options || {};
+          options = extend({ templateUrl: defaultTemplateUrl }, options);
+          options.tether = extend({}, defaultTetherOptions, options.tether || {});
 
-            var template = options.template || $templateCache.get(options.templateUrl),
-                    scope        = options.scope || $rootScope.$new(),
-                    target     = options.target,
-                    elem         = $compile(template)(scope),
-                    tether;
+          var template = options.template || $templateCache.get(options.templateUrl),
+            scope        = options.scope || $rootScope.$new(),
+            target     = options.target,
+            elem         = $compile(template)(scope),
+            tether;
 
-            /**
-             * Attach a tether to the tooltip and the target element.
-             */
-            function attachTether() {
-                new Tether(extend({
-                    element: elem,
-                    target: target
-                }, options.tether));
-            };
+          /**
+           * Attach a tether to the tooltip and the target element.
+           */
+          function attachTether() {
+            new Tether(extend({
+              element: elem,
+              target: target
+            }, options.tether));
+          };
 
-            /**
-             * Detach the tether.
-             */
-            function detachTether() {
-                if (tether) {
-                    tether.destroy();
-                }
-            };
+          /**
+           * Detach the tether.
+           */
+          function detachTether() {
+            if (tether) {
+              tether.destroy();
+            }
+          };
 
-            /**
-             * Open the tooltip
-             */
-            function open() {
-                $animate.enter(elem, null, target);
-                attachTether();
-            };
+          /**
+           * Open the tooltip
+           */
+          function open() {
+            $animate.enter(elem, null, target);
+            attachTether();
+          };
 
-            /**
-             * Close the tooltip
-             */
-            function close() {
-                $animate.leave(elem);
-                detachTether();
-            };
+          /**
+           * Close the tooltip
+           */
+          function close() {
+            $animate.leave(elem);
+            detachTether();
+          };
 
-            // Close the tooltip when the scope is destroyed.
-            scope.$on('$destroy', close);
+          // Close the tooltip when the scope is destroyed.
+          scope.$on('$destroy', close);
 
-            return {
-                open: open,
-                close: close
-            };
+          return {
+            open: open,
+            close: close
+          };
         };
-    }
-});
+      }]
+    })
 
-module.provider('$tooltipDirective', function() {
+    .provider('$tooltipDirective', function() {
 
-    /**
-     * Returns a factory function for building a directive for tooltips.
-     *
-     * @param {String} name - The name of the directive.
-     */
-    this.$get = function($tooltip) {
+      /**
+       * Returns a factory function for building a directive for tooltips.
+       *
+       * @param {String} name - The name of the directive.
+       */
+      this.$get = ['$tooltip', 'safeApply', function($tooltip, safeApply) {
         return function(name, options) {
-            return {
-                restrict: 'EA',
-                scope: {
-                    content:    '@' + name,
-                    tether:    '=?' + name + 'Tether'
-                },
-                link: function(scope, elem, attrs) {
-                    var tooltip = $tooltip(extend({
-                        target: elem,
-                        scope: scope
-                    }, options, { tether: scope.tether }));
+          return {
+            restrict: 'EA',
+            scope: {
+              content:    '=' + name,
+              tether:    '=?' + name + 'Tether',
+              binding:   '@?' + name + 'Binding'
+            },
+            link: function(scope, elem, attrs) {
+              var tooltip = $tooltip(extend({
+                target: elem,
+                scope: scope
+              }, options, { tether: scope.tether }));
 
-                    /**
-                     * Toggle the tooltip.
-                     */
-                    elem.hover(function() {
-                        scope.$apply(tooltip.open);
-                    }, function() {
-                        scope.$apply(tooltip.close);
-                    });
-                }
-            };
+
+              if (scope.binding === null) {
+                scope.binding = 'hover';
+              }
+
+              if (scope.binding === 'hover') {
+                /**
+                 * Toggle the tooltip.
+                 */
+                elem.hover(function() {
+                  safeApply(scope, tooltip.open);
+                }, function() {
+                  safeApply(scope, tooltip.close);
+                });
+              } else if (scope.binding === 'content') {
+                scope.contentChanged = function() {
+                  return scope.content;
+                };
+
+                scope.$watch('contentChanged()', function() {
+                  if (scope.content) {
+                    safeApply(scope, tooltip.close);
+                    safeApply(scope, tooltip.open);
+                  } else {
+                    safeApply(scope, tooltip.close);
+                  }
+                }, true);
+              }
+            }
+          };
         };
-    };
-});
+      }];
+    })
 
-module.directive('ngTooltip', function($tooltipDirective) {
-    return $tooltipDirective('ngTooltip');
-});
+    .directive('ngTooltip', ['$tooltipDirective', function($tooltipDirective) {
+      return $tooltipDirective('ngTooltip');
+    }])
 
-module.run(function($templateCache) {
-    $templateCache.put('template/ng-tooltip.html', '<div class="js-tooltip">{{content}}</div>');
-});
+    .run(['$templateCache', function($templateCache) {
+      $templateCache.put('template/ng-tooltip.html', '<div class="js-tooltip tether-theme-basic"><div class="tether-content">{{content}}</div></div>');
+    }]);
+
+})(window, window.angular);
